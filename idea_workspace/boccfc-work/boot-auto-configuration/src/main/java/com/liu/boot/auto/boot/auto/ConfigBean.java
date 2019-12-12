4f9dcfe912b4587b991889e6574b2f9b6ff13268
liuwa <liuwa@boccfctest.local>
2019-12-12 15:50:18 +0800

    redis

--- idea_workspace/boccfc-work/boot-auto-configuration/src/main/java/com/liu/boot/auto/boot/auto/ConfigBean.java
+++ idea_workspace/boccfc-work/boot-auto-configuration/src/main/java/com/liu/boot/auto/boot/auto/ConfigBean.java
@@ -1,5 +1,8 @@
 package com.liu.boot.auto.boot.auto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConfigBean {
 
     private StudentBean studentBean;
@@ -11,5 +14,15 @@ public class ConfigBean {
 
     public StudentBean getStudentBean() {
         return studentBean;
+    }
+
+    public static void main(String[] args) {
+        List<String> test = new ArrayList<>();
+        test.add("1");
+        test.add("2");
+        test.add("3");
+        test.add("4");
+        List<String> test1 = test.subList(1, 3);
+        System.out.println(test1);
     }
 }
