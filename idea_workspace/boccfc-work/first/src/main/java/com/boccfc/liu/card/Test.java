3e95499f08ebf1edb9e2b38b353aa66bdcca21b0
liuwa <liuwa@boccfctest.local>
2019-12-24 18:35:42 +0800

    tomcat

---
+++ idea_workspace/boccfc-work/first/src/main/java/com/boccfc/liu/card/Test.java
@@ -0,0 +1,19 @@
+package com.boccfc.liu.card;
+
+/**
+ * @author Liuweian
+ * @version 1.0.0
+ * @desc
+ * @createTime 2019/12/23 19:28
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int[] liu = new int[5];
+        liu[0] = 50;
+        liu[0]++;
+        System.out.println(liu[0]);
+
+    }
+
+}
